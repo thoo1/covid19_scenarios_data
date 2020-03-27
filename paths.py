ff0a094af6e28b2a55a7321535beb5d11b96f976
Nils Ole Tippenhauer <noleti@gmx.de>
2020-03-27 12:48:29 +0100

    first draft to add 'hemisphere' key to population.json for the Epidemiology. Mapping was manual, might be incorrect

--- paths.py
+++ paths.py
@@ -4,3 +4,4 @@ SOURCES_FILE = 'sources.json'
 TMP_CASES = 'case_counts.json'
 TMP_POPULATION = 'population.json'
 TMP_SCENARIOS = "scenario_defaults.json"
+TMP_CODES = 'country_codes.csv'
--- scripts/populations.py
+++ scripts/populations.py
@@ -20,7 +20,7 @@ import numpy as np
 import os
 import sys
 sys.path.append('..')
-from paths import TMP_CASES, BASE_PATH, JSON_DIR, TMP_POPULATION
+from paths import TMP_CASES, BASE_PATH, JSON_DIR, TMP_POPULATION, TMP_CODES
 
 # obsolete
 def getImportsPerDay(pop, cases):
@@ -30,7 +30,7 @@ def getImportsPerDay(pop, cases):
 def getCountryAbbreviations():
     toThreeLetter = {}
     toName = {}
-    with open(os.path.join(BASE_PATH, 'country_codes.csv')) as fh:
+    with open(os.path.join(BASE_PATH, TMP_CODES)) as fh:
         header = [x.strip('"') for x in fh.readline().strip().split(',')]
         name_index = header.index('name')
         three_letter_index = header.index('alpha-3')
@@ -43,6 +43,38 @@ def getCountryAbbreviations():
                 toName[entries[three_letter_index]] = entries[name_index]
 
     return toThreeLetter, toName
+
+# estimate hemisphere based on country_codes
+def getHemispheres():
+    toHemisphere = {}
+    mn = 'Moderate/North'
+    ms = 'Moderate/South'
+    eq = 'Moderate/Tropical'
+    hemiDict = {}
+    # to make this less awkward to fill
+    hemiDict[mn]= ['Northern Europe', 'Northern Africa', 'Western Europe', 'Southern Europe', 'Northern America', 'Eastern Europe', 'Eastern Asia', 'Western Asia', 'Central Asia']
+    hemiDict[ms]= ['Australia and New Zealand']
+    hemiDict[eq]= ['Southern Asia', 'Polynesia', 'Latin America and the Caribbean', 'Melanesia', 'Micronesia', 'Sub-Saharan Africa', 'South-eastern Asia']
+
+    hemiDict2 = {}
+    for i in [mn, ms, eq]:
+        for j in hemiDict[i]:
+            hemiDict2[j] = i
+    
+    print(hemiDict2)
+    with open(os.path.join(BASE_PATH, TMP_CODES)) as fh:
+        header = [x.strip('"') for x in fh.readline().strip().split(',')]
+        name_index = header.index('name')
+        sub_region_index = header.index('sub-region')
+        for entries in  csv.reader(fh, quotechar='"', delimiter=',',
+                     quoting=csv.QUOTE_ALL, skipinitialspace=True):        
+            if len(entries) > sub_region_index:
+                #if (entries[sub_region_index] not in hemiDict[mn]) and (entries[sub_region_index] not in hemiDict[ms]) and (entries[sub_region_index] not in hemiDict[eq]):
+                #    print(entries[sub_region_index])
+                if not entries[name_index] == 'Antarctica': 
+                    toHemisphere[entries[name_index]] = hemiDict2[entries[sub_region_index]]
+
+    return toHemisphere
 
 def dumpPopTable(pops, fname):
     with open(fname, 'w') as fh:
@@ -87,7 +119,14 @@ def parse():
     for d in pops:
         d['data']['cases'] = d['name'] if d['name'] in regions else 'none'
 
+    # Set epidemiology based on country_codes
+    hemispheres = getHemispheres()    
+    for d in pops:
+        d['data']['hemisphere'] = hemispheres[d['name']] if d['name'] in hemispheres else 'none'
+    
+
     with open(os.path.join(BASE_PATH, JSON_DIR,TMP_POPULATION), 'w') as fh:
+        print('dumping to ',os.path.join(BASE_PATH, JSON_DIR,TMP_POPULATION))
         json.dump(pops, fh)
 
 
